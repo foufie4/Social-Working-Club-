e3e9572369e9fb8d4a68140a3106e08274170fa3
Sophie <shamalow423@gmail.com>
2024-06-05 18:10:08 +0200

    modify the dropdown modif and supp on every posts

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,5 +7,7 @@ router.post('/create', authenticateJWT, PostController.createPost);
 router.get('/', PostController.getPosts);
 router.post('/:id/like', authenticateJWT, PostController.likePost);
 router.post('/:id/comment', authenticateJWT, PostController.commentPost);
+router.put('/:id', authenticateJWT, PostController.updatePost); 
+router.delete('/:id', authenticateJWT, PostController.deletePost); 
 
 module.exports = router;
